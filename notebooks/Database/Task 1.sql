31cba7c80e35641368e74676756b8622f3e52b63
pyhon Mohsen <mohsen_mohebbi@ymail.com>
2025-01-25 19:15:41 +0330

    How many times has each product been ordered? Please include the product name in the table.

--- notebooks/Database/Task 1.sql
+++ notebooks/Database/Task 1.sql
@@ -4,3 +4,9 @@ FROM customers
 GROUP BY country
 ORDER BY customer_count DESC
 limit 5;
+
+# How many times has each product been ordered? Please include the product name in the table.
+SELECT p.productCode, p.productName, SUM(od.quantityOrdered) AS total_ordered
+FROM orderdetails od
+JOIN products p ON od.productCode = p.productCode
+GROUP BY p.productCode, p.productName;
