7785b0b30216942ece64a5d86c3de0cfc858f826
pyhon Mohsen <mohsen_mohebbi@ymail.com>
2025-01-25 19:01:31 +0330

    How many customers are there in each country? Which are the top 5 countries?

--- notebooks/Database/Task 1.sql
+++ notebooks/Database/Task 1.sql
@@ -1,3 +1,6 @@
-# How many customers are there in each country?
-SELECT country, COUNT(*) AS customer_count FROM customers
-GROUP BY country;
+# How many customers are there in each country? Which are the top 5 countries?
+SELECT country, COUNT(*) AS customer_count
+FROM customers
+GROUP BY country
+ORDER BY customer_count DESC
+limit 5;
