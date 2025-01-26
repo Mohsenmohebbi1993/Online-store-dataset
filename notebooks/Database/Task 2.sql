8c76e26aac5466363a316cb46207faaa8063d25f
pyhon Mohsen <mohsen_mohebbi@ymail.com>
2025-01-26 19:36:54 +0330

    How many Quantity of products has each customer purchased?

--- notebooks/Database/Task 2.sql
+++ notebooks/Database/Task 2.sql
@@ -2,3 +2,28 @@
 SELECT customerName, city, country
 FROM customers
 WHERE country = 'USA';
+
+# List of all payments made by customer 103 (customerNumber = 103)
+SELECT checkNumber, paymentDate, amount
+FROM payments
+WHERE customerNumber = 103;
+
+# How many payments has each customer made? Include the customer's name
+SELECT p.customerNumber, c.customerName, count(*) AS count_order
+FROM payments p
+join customers c on c.customerNumber = p.customerNumber
+group by p.customerNumber, c.customerName
+order by count_order desc;
+
+# How many Quantity of products has each customer purchased?
+select t.customerNumber, t.customerName, sum(quantity_Ordered)
+from(select os.customerNumber,
+	   c.customerName,
+       o.orderNumber,
+       sum(quantityOrdered) as quantity_Ordered
+from orderdetails o
+join orders os on os.orderNumber = o.orderNumber
+join customers c on c.customerNumber = os.customerNumber
+group by orderNumber) as t
+group by t.customerNumber, t.customerName
+order by sum(quantity_Ordered) desc;
